3aceed524ebf54577bf52978a55c561e9bafeb57
Gu Tianqi <ntpeter164@126.com>
2019-05-22 21:22:11 +0800

    更改架构

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,31 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Door : MonoBehaviour
+public class Door : MonoBehaviour
 {
     public GameObject doorParent; //door所创建的新房间对应的所有门的parent
     public GameManager g;
+    public enum DoorColor
+    {
+        red, green, purple
+    }
+    public DoorColor doorColor;
     public void Start()
     {
         g = GameObject.FindObjectOfType<GameManager>();
     }
-    public abstract void OnTriggerEnter(Collider collider);//改变房间以及墙壁碰撞
-}
-public class PurpleDoor : Door
-{
-    public PurpleDoor(GameObject doorParent)
+    public void OnTriggerEnter(Collider collider)//改变房间以及墙壁碰撞
     {
-        this.doorParent = doorParent;
+        //switch..case..
+        purpleChange();
     }
-    public override void OnTriggerEnter(Collider collider)
+    void purpleChange()
     {
-        change();
-    }
-    void change()
-    {
-        if(g.cr==GameManager.currentRoom.room1)
+        if (g.cr == GameManager.currentRoom.room1)
         {
-            g.room2.gameObject.transform.position = g.room1.transform.position 
+            g.room2.gameObject.transform.position = g.room1.transform.position
                 + new Vector3(transform.up.x * g.moveDistace.x, transform.up.y * g.moveDistace.y, transform.up.z * g.moveDistace.z); //moveDistance为房间长宽高
         }
         else
@@ -36,4 +34,3 @@ public class PurpleDoor : Door
         }
     }
 }
-
