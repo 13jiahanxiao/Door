3df72b10204f3e7507ef201d0cff70efcfe07a87
Gu Tianqi <ntpeter164@126.com>
2019-06-12 18:19:54 +0800

    4.5

---
+++ Assets/Scripts/ItemCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollider : MonoBehaviour
+{
+    private Quaternion rot;
+    private Vector3 pos;
+    void OnTriggerEnter(Collider collider)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+}
+
